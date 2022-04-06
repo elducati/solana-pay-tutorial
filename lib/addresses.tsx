ef5b0e9dd08f6712dec66e7c3b073e5f85948f26
elducati <geomondi09@gmail.com>
2022-04-06 22:20:09 +0300

    add coupon feature

--- lib/addresses.tsx
+++ lib/addresses.tsx
@@ -4,3 +4,5 @@ import { PublicKey } from "@solana/web3.js"
 export const shopAddress = new PublicKey('2vxUwg4cF8Wv4y4Jur8bSmib9f8S4ajovxph495PmDkB') 
 // this is the same for everyone!
 export const usdcAddress = new PublicKey('Gh9ZwEmdLJ8DscKNTkTqPbNwLNNBjuSzaG9Vp2KGtKJr')
+// This is your token/coupon address
+export const couponAddress = new PublicKey('HNKZ3AvB3eV5ihpxoqvXeo3UXsHwnHtdcmzCAG6iLXiD')
