685821a1cf3836dd947a58d228cdbb775067fea3
elducati <geomondi09@gmail.com>
2022-04-06 21:32:52 +0300

    update transaction requests

--- lib/addresses.tsx
+++ lib/addresses.tsx
@@ -1,6 +1,6 @@
 import { PublicKey } from "@solana/web3.js"
 
 // Your shop wallet address
-export const shopAddress = new PublicKey('...') 
+export const shopAddress = new PublicKey('2vxUwg4cF8Wv4y4Jur8bSmib9f8S4ajovxph495PmDkB') 
 // this is the same for everyone!
 export const usdcAddress = new PublicKey('Gh9ZwEmdLJ8DscKNTkTqPbNwLNNBjuSzaG9Vp2KGtKJr')
--- pages/shop/index.tsx
+++ pages/shop/index.tsx
@@ -4,7 +4,7 @@ import SiteHeading from '../../components/SiteHeading'
 export default function ShopPage() {
   return (
     <div className="flex flex-col gap-8 max-w-4xl items-stretch m-auto pt-24">
-      <SiteHeading>Cookies Inc</SiteHeading>
+      <SiteHeading>El Ducati Inc</SiteHeading>
       <Products submitTarget='/shop/checkout' enabled={true} />    </div>
   )
 }
